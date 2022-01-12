2b7cd3eee7ef4422cb9ccacc36d1b8503e4424d5
korann18 <60140258+korann18@users.noreply.github.com>
2022-01-12 21:46:35 +0200

    commit 1.1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import include, path
-
 from rest_framework import routers
-
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     ReviewViewSet, TitleViewSet, UserViewSet, get_auth_token,
                     signup)
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,15 +3,12 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-
 from reviews.models import Category, Genre, Review, Title
 from users.models import User, UserRoles
-
 from .filters import TitlesFilter
 from .mixins import CustomViewSet
 from .permissions import IsAdmin, IsAdminOrReadOnly, ReviewCommentPermission
