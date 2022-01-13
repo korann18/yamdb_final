1c81f240acd72c3b26b55b4c48ad88d54b10fb19
korann18 <60140258+korann18@users.noreply.github.com>
2022-01-13 10:34:39 +0200

    commit 1.4

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from .validators import year_validator
+from reviews.validators import year_validator
 
 User = get_user_model()
 
